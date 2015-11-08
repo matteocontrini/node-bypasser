3dc4fe30922aa88acf6fef1f610ee250b07d7d3b
Khoa Nguyen <khoa02031995@gmail.com>
2015-11-09 04:42:59 +0700

    Add generic service

---
+++ services/__generic.js
@@ -0,0 +1,29 @@
+/*
+	Generic / Unknown services
+ */
+
+var request = require('request');
+
+var Service = require('../Service.js');
+var common  = require('../common.js');
+
+var service = new Service("Generic");
+service.hosts = [];
+
+service.run = function(url, callback) {
+	var options = {
+		url: url,
+		followRedirect: false
+	};
+	
+	request(options, function(error, response, body) {
+		if (error || [301,302].indexOf(response.statusCode) == -1) {
+      callback('URL not recognized as supported');
+      return;
+		}
+		
+		callback(null, response.headers.location);
+	});
+};
+
+module.exports = service;
