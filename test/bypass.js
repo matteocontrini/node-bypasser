16398367a394d7e227ba781d1ba87dacd7df6080
Matteo Contrini <m.contrini@gmail.com>
2015-11-08 23:01:45 +0100

    Some unit tests

---
+++ test/bypass.js
@@ -0,0 +1,70 @@
+var expect = require('chai').expect;
+
+var Bypasser = require('../index.js');
+
+describe('Decrypt', function() {
+	this.timeout(0);
+	
+	it('Adf.ly', function(done) {
+		var b = new Bypasser('http://adf.ly/pVVXg');
+		b.decrypt(function(err, result) {
+			expect(err).to.be.null;
+			expect(result).to.equal('http://github.com');
+			done();
+		});
+	});
+	
+	it('Linkbucks', function(done) {
+		var b = new Bypasser('http://www.linkbucks.com/Kutv');
+		b.decrypt(function(err, result) {
+			expect(err).to.be.null;
+			expect(result).to.equal('http://github.com');
+			done();
+		});
+	});
+	
+	it('Shorte.st', function(done) {
+		var b = new Bypasser('http://sh.st/gS8XD');
+		b.decrypt(function(err, result) {
+			expect(err).to.be.null;
+			expect(result).to.equal('http://github.com');
+			done();
+		});
+	});
+	
+	it('AdFoc.us', function(done) {
+		var b = new Bypasser('http://adfoc.us/x57045654');
+		b.decrypt(function(err, result) {
+			expect(err).to.be.null;
+			expect(result).to.equal('http://github.com');
+			done();
+		});
+	});
+	
+	it('Generic goo.gl', function(done) {
+		var b = new Bypasser('http://goo.gl/NWt4Es');
+		b.decrypt(function(err, result) {
+			expect(err).to.be.null;
+			expect(result).to.equal('http://github.com/');
+			done();
+		});
+	});
+	
+	it('NowVideo', function(done) {
+		var b = new Bypasser('http://www.nowvideo.li/video/6b3032d85873d');
+		b.decrypt(function(err, result) {
+			expect(err).to.be.null;
+			expect(result.slice(-4)).to.equal('.flv');
+			done();
+		});
+	});
+	
+	it('NovaMov', function(done) {
+		var b = new Bypasser('http://www.novamov.com/video/4bb1ba486069a');
+		b.decrypt(function(err, result) {
+			expect(err).to.be.null;
+			expect(result.slice(-4)).to.equal('.flv');
+			done();
+		});
+	});
+});
---
+++ test/match.js
@@ -0,0 +1,40 @@
+var expect = require('chai').expect;
+
+var Bypasser = require('../index.js');
+
+describe('Match URL to host', function() {
+	it('Adf.ly', function() {
+		var b = new Bypasser('http://adf.ly/pVVXg');
+		expect(b.service.name).to.equal('Adf.ly');
+	});
+	
+	it('Linkbucks', function() {
+		var b = new Bypasser('http://www.linkbucks.com/Kutv');
+		expect(b.service.name).to.equal('Linkbucks');
+	});
+	
+	it('Shorte.st', function() {
+		var b = new Bypasser('http://sh.st/gS8XD');
+		expect(b.service.name).to.equal('Shorte.st');
+	});
+	
+	it('AdFoc.us', function() {
+		var b = new Bypasser('http://adfoc.us/x57045654');
+		expect(b.service.name).to.equal('AdFoc.us');
+	});
+	
+	it('Generic goo.gl', function() {
+		var b = new Bypasser('http://goo.gl/NWt4Es');
+		expect(b.service.name).to.equal('Generic');
+	});
+	
+	it('NowVideo', function() {
+		var b = new Bypasser('http://www.nowvideo.li/video/6b3032d85873d');
+		expect(b.service.name).to.equal('NowVideo');
+	});
+	
+	it('NovaMov', function() {
+		var b = new Bypasser('http://www.novamov.com/video/t1j3cls50rumq');
+		expect(b.service.name).to.equal('NowVideo');
+	});
+});
