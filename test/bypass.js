7cedb518bf5535ed7f964b863cb0d0b6a11dde7d
Khoa Nguyen <khoa02031995@gmail.com>
2015-11-11 03:07:03 +0700

    Indentation again :-1:

--- test/bypass.js
+++ test/bypass.js
@@ -123,11 +123,12 @@ describe('Decrypt', function() {
 		});
 	});
 
-  	it('Throw error when receive invalid URL', function(done) {
-    	var b = new Bypasser('github.com');
-    	b.decrypt(function(err, result) {
-      	expect(err).to.equal('This is not a valid url');
-      	done();
-    });
-  });
+	it('Throw error when receive invalid URL', function(done) {
+		var b = new Bypasser('github.com');
+		b.decrypt(function(err, result) {
+			expect(result).to.be.null;
+			expect(err).to.equal('This is not a valid url');
+			done();
+		});
+	});
 });
