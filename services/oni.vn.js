5b0f4533f9ae64cc324be87617af486ac194d129
Khoa Nguyen <khoa02031995@gmail.com>
2015-11-12 04:55:51 +0700

    Make oni.vn work

--- services/oni.vn.js
+++ services/oni.vn.js
@@ -1,44 +1,28 @@
 /*
-  P.pw
-  Hosts: p.pw
+ Oni.vn
+ Hosts: oni.vn
  */
 
 var request = require('request');
 var Service = require('../service.js');
 var service = new Service('Oni.vn');
+var cheerio = require('cheerio');
+
 service.hosts = ['oni.vn'];
 
 service.run = function(url, callback) {
 
-	var options = {
-		url: url
-	};
-
-	request = request.defaults({ jar: true });
-
-	request(options, function(error, response, body) {
+	request(url, function(error, response, body) {
 		if (error || response.statusCode != 200) {
 			callback('Unexpected response status code. Response code: ' + response.statusCode);
 			return;
 		}
-		
-		var data = body.match(/data:"(.*)",/)[1];
 
-		console.log(data);
-		
-		options = {
-			url: 'http://www.oni.vn/click.html?' + data,
-			headers: {
-				'Content-Type':'application/json; charset=utf-8',
-				Host: 'www.oni.vn',
-				Referer: url
-			}
-		};
+		var $ = cheerio.load(body);
 
-		request(options, function(error, response, body) {
-			console.log(body);
-		});
-		//console.log(parsedUrl);
+		var link = $('meta[property="og:url"]').attr('content');
+
+		callback(null, link);
 	});
 };
 
