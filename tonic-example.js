9fccb3893cb1578846f26f4187b9fee6ab9df508
Matteo Contrini <m.contrini@gmail.com>
2015-11-12 17:04:40 +0100

    Lint in Tonic example

--- tonic-example.js
+++ tonic-example.js
@@ -2,5 +2,10 @@ var Bypasser = require('node-bypasser');
 
 var w = new Bypasser('http://adf.ly/1DX0XD');
 w.decrypt(function(err, result) {
+	if (err) {
+		console.log(err);
+		return;
+	}
+	
 	console.log('Decrypted: ' + result);
 });
